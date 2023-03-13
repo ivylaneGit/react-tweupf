f46b02449d1ac647ce6d99f13231c810012220b7
ivylaneGit <109193229+ivylaneGit@users.noreply.github.com>
2023-03-13 15:54:34 +0800

    3 files modified, 1 deleted

---
+++ src/Page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './Header' 
+
+export default function() {
+  const name = 'John';
+  return (
+    <>
+      <Header name={} />
+      <p>Hey</p>
+      <p>Hi</p>
+    </>
+  );
+}
+
+const age = 10;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import Page from './Page';
 import './style.css';
-
-function Header(props) {
-  const clock = Date().toLocaleString();
-  return (
-    <h1 className="orange">
-      Hello {props.name} {clock} 
-    </h1>
-  );
-}
-
-function Page() {
-  const name = 'John';
-  return (
-    <>
-      <Header name={} />
-      <p>Hey</p>
-      <p>Hi</p>
-    </>
-  );
-}
 
 const rootEl = document.querySelector('#root');
 const root = ReactDom.createRoot(rootEl);
